0056bfc28f1000ebd9e17193bfc5567bad3c25d2
abdelazizade <abdealziz.adel.m@gmail.com>
2024-04-04 01:14:12 +0200

    handle guard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,38 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements CanActivate {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService,private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (true) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
 
   onSubmit() {
-    this.authService.login(this.email, this.password)
-      .subscribe(
-        response => {
-          const token = response.body.token;
-          localStorage.setItem('token', token);
-          console.log('Token:', token);
-          this.router.navigate(['/products']);        },
-        error => {
-          console.log(error);
-        }
-      );
+    this.authService.login(this.email, this.password).subscribe(
+      (response) => {
+        const token = response.body.token;
+        localStorage.setItem('token', token);
+        console.log('Token:', token);
+        this.router.navigate(['/products']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
